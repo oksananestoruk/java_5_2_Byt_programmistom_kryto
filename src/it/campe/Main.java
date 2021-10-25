9a610225d1dd655371c3d81eae676c277b0dc469
oksananestoruk <osivchenko@gmail.com>
2021-10-25 13:07:33 +0200

    task complete

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         //напишите тут ваш код
+        System.out.print("Я ");
+        System.out.print("думаю, ");
+        System.out.print("быть ");
+        System.out.print("программистом ");
+        System.out.print("круто.");
+
     }
 }
