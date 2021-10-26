17978c01ee9a50aee5f4279db2a4ef03225ddc67
oksananestoruk <osivchenko@gmail.com>
2021-10-26 10:03:29 +0200

    task complete

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -4,11 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         //напишите тут ваш код
-        System.out.print("Я ");
-        System.out.print("думаю, ");
-        System.out.print("быть ");
-        System.out.print("программистом ");
-        System.out.print("круто.");
+        System.out.print("Я думаю, быть программистом круто.");
 
     }
 }
